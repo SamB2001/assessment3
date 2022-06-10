e9a0ab8cddedcc28e1a4299a543604175a3cfda4
Sam Bischoff <sam.bischoff2001@gmail.com>
2022-06-10 11:27:13 -0600

    main portion complete, going to attempt EC

--- assessment-front-end-web/profile.js
+++ assessment-front-end-web/profile.js
@@ -1,1 +1,34 @@
-console.log('hey');
+// console.log('hey');
+function handleSubmit(evt) {
+	evt.preventDefault();
+	
+	window.alert('Form Submitted!');
+}
+
+function color(evt){
+    evt.preventDefault();
+
+    window.alert('My favorite color is red!')
+}
+
+function place(evt){
+    evt.preventDefault();
+
+    window.alert('My favorite place is Washington State!')
+}
+
+function ritual(evt){
+    evt.preventDefault();
+
+    window.alert('My favorite ritual is going to the gym first thing in the morning!')
+}
+
+let colorBtn = document.querySelector('#color')
+let placeBtn = document.querySelector('#place')
+let ritualBtn = document.querySelector('#ritual')
+let hire = document.querySelector('#hire')
+
+colorBtn.addEventListener('click', color)
+placeBtn.addEventListener('click', place)
+ritualBtn.addEventListener('click', ritual)
+hire.addEventListener('submit', handleSubmit);
